6978cff37600e5e37f42f5f1ea25879834389b35
gaaurav0208 <68295108+gaaurav0208@users.noreply.github.com>
2025-05-02 18:43:33 +0530

    Update apiClient.js

--- src/app/services/apiClient.js
+++ src/app/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://sipstr-vendor.vercel.app' || 'http://localhost:3001',
     headers: {
       'Content-Type': 'application/json',
     },
